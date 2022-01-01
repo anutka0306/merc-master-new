a5a5cb1a684a405b4d0d3afe272c0745173a6bc5
Anna Kotelnikova <anutka0306@mail.ru>
2022-01-02 01:06:38 +0300

    Слайдер на главной - корректировки

--- public/js/custom.js
+++ public/js/custom.js
@@ -107,10 +107,46 @@ $(document).ready(function () {
 
     $('.slick__main').slick({
         infinite: true,
-        slidesToShow: 3,
+        slidesToShow: 6,
         slidesToScroll: 1,
         autoplay: true,
         arrows: false,
+        responsive: [
+            {
+                breakpoint: 1366,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+
+            {
+                breakpoint: 880,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 
 });
