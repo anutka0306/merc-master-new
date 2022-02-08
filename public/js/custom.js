ef65db7939cb70bcf2b728dfdc2557a173118b4b
Anna Kotelnikova <anutka0306@mail.ru>
2022-02-08 21:54:54 +0300

    magnific-popup для slick__main

--- public/js/custom.js
+++ public/js/custom.js
@@ -110,7 +110,7 @@ $(document).ready(function () {
         slidesToShow: 6,
         slidesToScroll: 1,
         autoplay: true,
-        arrows: false,
+        arrows: true,
         responsive: [
             {
                 breakpoint: 1366,
@@ -147,6 +147,29 @@ $(document).ready(function () {
             // settings: "unslick"
             // instead of a settings object
         ]
+    });
+
+    $('.slick__main').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        closeOnContentClick: false,
+        closeBtnInside: false,
+        mainClass: 'mfp-with-zoom mfp-img-mobile',
+        image: {
+            verticalFit: true,
+
+        },
+        gallery: {
+            enabled: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300, // don't foget to change the duration also in CSS
+            opener: function(element) {
+                return element.find('img');
+            }
+        }
+
     });
 
 });
